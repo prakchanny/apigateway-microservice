d0d07c6eadcf605e3021718d6ae39c937929714a
prakchanny <136786242+prakchanny@users.noreply.github.com>
2025-05-30 16:18:40 +0700

    Update apigateway.js

--- apigateway.js
+++ apigateway.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer({});
 //REDIRECT TO STUDENT MICROSERVICE
 app.use('/student', (req, res) => {
     console.log('Inside API GATEWAY STUDENT ROUTE');
-    proxy.web(req, res, { target: 'http://localhost:4000' });
+    proxy.web(req, res, { target: 'http://3.95.221.5:4000' });
 });
 
 //REDIRECT TO TEACHER MICROSERVICE
 app.use('/teacher', (req, res) => {
-        console.log('Inside API GATEWAY TEACHER ROUTE');
-        proxy.web(req, res, { target: 'http://localhost:3000' });
+    console.log('Inside API GATEWAY TEACHER ROUTE');
+    proxy.web(req, res, { target: 'http://44.210.128.217:3000' });
 });
 
 app.listen(5000, () => {
